73186c38fa0aa955be0c1750552d24fd48b211cb
Samuel Porto Chaves <33035760+Samchaves@users.noreply.github.com>
2021-06-22 15:53:50 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata";
 import express, { request, response } from "express";
 import "./database";
+import { router } from "./services/routes";
 
 
 // @types/expless
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 // http: //localhost: 3000
 app.listen(3000, () => console.log("Server is running"));
